433cae76750ed54ab7dd2700dc678a95d0b56520
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 17:18:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,9 @@ class Student
     SELECT * FROM students
     WHERE grade = 10 LIMIT 1
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row| 
+      self.new_from_db(row)
+    end.first
   end
 
 end
