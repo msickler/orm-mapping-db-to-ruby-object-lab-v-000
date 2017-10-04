4eed2c9010678743033a0337b163650ab0344647
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 17:09:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,4 +60,14 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def self.students_below_12th_grade
+    sql = <<-SQL 
+    SELECT name FROM students 
+    WHERE grade < 12
+    SQL 
+    DB[:conn].execute(sql)
+  end
+
+  
+
 end
