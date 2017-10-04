6ff17551173accbbefcbff504e71070272c15513
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-04 17:17:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,8 +75,8 @@ class Student
     DB[:conn].execute(sql, x)
   end
 
-  def self.first_student_in_grade_10 
-    sql = <<-SQL 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10 LIMIT 1
     SQL
